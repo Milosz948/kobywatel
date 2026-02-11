562ae25543170a80785d202c7ff796a5f1744018
Milosz948 <milosz@wagrodzki.int.pl>
2026-02-11 19:12:18 +0100

    Update qr.js

--- assets/qr.js
+++ assets/qr.js
@@ -11,4 +11,23 @@ document.querySelectorAll(".close").forEach((element) => {
     element.addEventListener('click', () => {
         error.classList.remove("error_open");
     })
+
 })
+let seconds = 150;
+const timer = document.getElementById("qrTimer");
+const progress = document.getElementById("qrProgress");
+
+const interval = setInterval(() => {
+    if (seconds <= 0) {
+        clearInterval(interval);
+        return;
+    }
+
+    const min = Math.floor(seconds / 60);
+    const sec = seconds % 60;
+
+    timer.textContent = `Kod wygaśnie za: ${min}:${sec.toString().padStart(2,"0")}`;
+    progress.value = (seconds / 150) * 100;
+
+    seconds--;
+}, 1000);
